4a7d0a033b183461d05c16c70421753d469be80d
Aram Zucker-Scharff <azuckers@gmu.edu>
2013-03-22 19:04:16 -0400

    setup to go

--- syllabi-to-course.php
+++ syllabi-to-course.php
@@ -9,7 +9,14 @@ $query = new WP_Query( array( 'post_type' => 'syllabus') );
  
 if ( $query->have_posts() ) : ?>
 	<?php while ( $query->have_posts() ) : $query->the_post();
-	
+		$id = get_the_ID();
+		$my_post = array();
+		$my_post['ID'] = $id;
+		$my_post['post_type'] = 'course';
+
+// Update the post into the database
+		wp_update_post( $my_post );	
+		
 	endwhile; wp_reset_postdata();
 	<!-- show pagination here -->
 <?php else : ?>
